e9301817b0cf45bb4fc862e8bf2beb8d34e1bc75
Sourabh4151 <sourabhtayal212@gmail.com>
2026-03-20 14:38:03 +0530

    Added favicon and updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import faviconUrl from './assets/cracked_favicon.png'
+
+const faviconLink = document.createElement('link')
+faviconLink.rel = 'icon'
+faviconLink.type = 'image/png'
+faviconLink.href = faviconUrl
+document.head.appendChild(faviconLink)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
