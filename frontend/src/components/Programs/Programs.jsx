2b7f64a23cbaf6e5bc57fbaf7d84506f29a5ba49
Sourabh4151 <sourabhtayal212@gmail.com>
2026-08-22 18:13:26 +0530

    Fix program link SEO text

--- frontend/src/components/Programs/Programs.jsx
+++ frontend/src/components/Programs/Programs.jsx
@@ -686,7 +686,25 @@ const Programs = () => {
                           aria-label={`Learn more about ${programCards[featuredCardIndex]?.title || currentProgramDetails.programLabel}`}
                           onClick={() => trackMicrositeClick(programCards[featuredCardIndex]?.title)}
                         >
-                          <button className="learn-more-button">Learn More</button>
+                          <button className="learn-more-button">
+                            Learn More
+                            <span
+                              aria-hidden="true"
+                              style={{
+                                position: 'absolute',
+                                width: 1,
+                                height: 1,
+                                padding: 0,
+                                margin: -1,
+                                overflow: 'hidden',
+                                clip: 'rect(0, 0, 0, 0)',
+                                whiteSpace: 'nowrap',
+                                border: 0,
+                              }}
+                            >
+                              {` about ${programCards[featuredCardIndex]?.title || currentProgramDetails.programLabel}`}
+                            </span>
+                          </button>
                         </a>
                       </div>
                     </div>
